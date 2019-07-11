c56d0e1367234c788aa60e17a9d8cbaea4cb6e78
Guillem <cabo.guillem@gmail.com>
2019-07-11 17:37:37 +0200

    WIP - First commit MCCU
    Verilator not compiling, logic not tested

---
+++ MCCU/runVerilatorTest.sh
@@ -0,0 +1,15 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${RED} Modify the script if you need to set your verilator path ${NC}"
+#____________start set path verilator
+#export TOP=/home/bscuser/BSC/lowrisc
+#export VERILATOR_ROOT=$TOP/verilator
+#____________end set path verilator
+rm -rf obj_dir 
+verilator -Wall --cc --trace MCCU.v --exe MCCU_TB.cpp -CFLAGS "-std=c++14"
+
+cd obj_dir/
+make -f VMCCU.mk 
+./VMCCU
+gtkwave VMCCU.vcd  ../tests.gtkw
