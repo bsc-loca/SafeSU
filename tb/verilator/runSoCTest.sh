8e5ac6a6b60f4b347a97ecb0a84fe5c2a43b48ee
Guillem <cabo.guillem@gmail.com>
2019-09-16 14:52:22 +0200

    updated c tests

--- tb/verilator/runSoCTest.sh
+++ tb/verilator/runSoCTest.sh
@@ -5,6 +5,10 @@ cd ../../../../
 cd vsim
 make clean
 make sim-lagarto
+#Copy most recent drivers
+cp $TOP/fpga/board/kc705/driver/PMU.c  $TOP/lagarto_modulos/AXI_PMU/tb/software_tests/pmu_sample_program/programs/common/ 
+cp $TOP/fpga/board/kc705/driver/PMU.h  $TOP/lagarto_modulos/AXI_PMU/tb/software_tests/pmu_sample_program/programs/common/ 
+cp $TOP/fpga/board/kc705/driver/PMU.o  $TOP/lagarto_modulos/AXI_PMU/tb/software_tests/pmu_sample_program/programs/ 
 # make our test
 cd $TOP/lagarto_modulos/AXI_PMU/tb/software_tests/pmu_sample_program/programs
 make clean
@@ -14,4 +18,4 @@ cd -
 #get waveform
 ./DefaultConfig-sim +vcd +vcd_name=write_test.vcd +max-cycles=10000  +load=./myWrite.riscv.hex | spike-dasm > write_test.log
 #display waveform
-gtkwave write_test.vcd gtkwave_configs/io_debug_PC_OP_FUNC_CLK_AXIMEM_AXIIO.gtkw
+gtkwave write_test.vcd $TOP/lagarto_modulos/AXI_PMU/tb/verilator/testSOC.gtkw
