8f74764919245bb941bc769bf1fe4cf866a5e37c
Guillem <cabo.guillem@gmail.com>
2020-06-11 12:10:32 +0200

    update folder structure

--- tb/questa_sim/tb_axi_pmu/runtest.sh
+++ tb/questa_sim/tb_axi_pmu/runtest.sh
@@ -1,9 +1,10 @@
 $1
 CYCLES=-all
+TOP=../../..
 mv /AXI_PMU /tmp
 vlib AXI_PMU
 vmap work $PWD/AXI_PMU
-vlog +acc=rn +incdir+../../hdl/ ../../hdl/*.sv ../../submodules/MCCU/hdl/* ../../submodules/RDC/hdl/*.sv  ./fifos/hdl/sync_fifo.v ./axi_test_master/hdl/axi_test_master.v tb_AXI_PMU.sv ./colors.vh
+vlog +acc=rn +incdir+$TOP/hdl/ $TOP/hdl/*.sv $TOP/submodules/MCCU/hdl/* $TOP/submodules/RDC/hdl/*.sv  ./fifos/hdl/sync_fifo.v ./axi_test_master/hdl/axi_test_master.v tb_AXI_PMU.sv ./colors.vh
 vmake AXI_PMU/ > Makefile
 if [ -z "$1" ]
 then
