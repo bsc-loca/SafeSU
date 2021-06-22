67bc278ea5d85c024a474facb3c809e8156523e5
GuillemCabo <cabo.guillem@gmail.com>
2021-06-22 13:23:59 +0200

    add questa ci script

--- tb/questa_sim/tb_axi_pmu/runtest.sh
+++ tb/questa_sim/tb_axi_pmu/runtest.sh
@@ -1,7 +1,6 @@
-$1
 CYCLES=-all
 TOP=../../..
-mv /AXI_PMU /tmp
+mv /AXI_PMU /tmp 2>/dev/null
 vlib AXI_PMU
 vmap work $PWD/AXI_PMU
 vlog +acc=rn +incdir+$TOP/hdl/ $TOP/hdl/*.sv $TOP/submodules/MCCU/hdl/* $TOP/submodules/RDC/hdl/*.sv  ./fifos/hdl/sync_fifo.v ./axi_test_master/hdl/axi_test_master.v tb_AXI_PMU.sv ./colors.vh
